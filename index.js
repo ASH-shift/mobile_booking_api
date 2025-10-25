8ef8398994b8ed1c7f646e796133172fd9e4a808
arpit sharma <arpit02456@gmail.com>
2025-10-25 16:05:03 +0530

    Features:basic express app with mongoose setup

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const env = require('dotenv');
+
+env.config(); // 
+
+const app = express();
+const PORT = process.env.PORT 
+const DB_URL = process.env.DB_URL;
+
+app.get('/home', (req, res) => {
+  console.log("hitting home");
+  return res.json({
+    success: true,
+    message: "fetched home"
+  });
+});
+
+mongoose.connect(DB_URL)
+  .then(() => {
+    console.log(" Mongoose connected");
+    app.listen(PORT, () => {
+      console.log(` Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(" Failed to connect to MongoDB", err);
+  });
